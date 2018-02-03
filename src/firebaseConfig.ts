b34da94c1821f4d418fee319ba55c49ea8faabe2
Invitado <1dludenarossy@gmail.com>
2018-02-03 12:08:59 -0500

    primer commit

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const config = {
+    apiKey: "AIzaSyCNmgXY1Nzolo6EjqBtw1zdbl5odhO3MNM",
+    authDomain: "rise-58db7.firebaseapp.com",
+    databaseURL: "https://rise-58db7.firebaseio.com",
+    projectId: "rise-58db7",
+    storageBucket: "rise-58db7.appspot.com",
+    messagingSenderId: "881177989537"
+
+}
---
+++ src/shared/preguntas.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import{ Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class PreguntasService {
+  preguntasReference:  AngularFireList<any>;
+  preguntasObservable: Observable<any>;
+
+  constructor(public db: AngularFireDatabase){
+    this.preguntasReference =db.list('preguntas');
+    this.preguntasObservable = this.preguntasReference.snapshotChanges()
+    .map(cambios => {
+      return cambios.map(cambio =>{
+        return cambio.paylod.val();
+      })
+    });
+
+  }
+
+
+}
