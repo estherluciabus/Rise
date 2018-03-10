f8e0ddae190f28375d5b59b0eccdc9f7f6dc85ae
salome <ghyu_evs@hotmail.com>
2018-03-10 00:36:28 -0500

    bloque

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,6 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+
 }
--- src/pages/prevencion/prevencion.ts
+++ src/pages/prevencion/prevencion.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
-
-
 @Component({
   selector: 'page-prevencion',
   templateUrl: 'prevencion.html'
 })
 export class PrevencionPage {
+
 
 
 
