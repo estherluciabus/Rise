d7a40a2090003b82d192e99160c81153cfc37f1d
salome <ghyu_evs@hotmail.com>
2018-04-24 18:30:51 -0500

    sunrise

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,29 +1,11 @@
-<<<<<<< HEAD
 import { ToastController } from 'ionic-angular';
 import { NavController ,AlertController } from 'ionic-angular';
 import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Component} from '@angular/core';
-=======
-<<<<<<< HEAD
- import { ToastController } from 'ionic-angular';
- import { Component } from '@angular/core';
- import { NavController } from 'ionic-angular';
- import { ToastController} from 'ionic-angular';
- import { FormControl} from "@angular/forms";
- import { NavController,ModalController } from 'ionic-angular';
- import {  Component, NgZone, ElementRef, OnInit, ViewChild} from '@angular/core';
- import {
-=======
-import { ToastController} from 'ionic-angular';
-import { FormControl} from "@angular/forms";
-import { NavController } from 'ionic-angular';
-import { Component, NgZone, ElementRef, ViewChild} from '@angular/core';
->>>>>>> c85444ad57ce97d6679775f34a4d334bc1448639
 import { BloqueadoresPage } from '../../pages/bloqueadores/bloqueadores';
 import {
->>>>>>> e48c753071cd6573d82d989912bb10c2128cfbb0
   GoogleMaps,
   GoogleMap,
   GoogleMapsEvent,
@@ -34,9 +16,6 @@ import { Geolocation } from '@ionic-native/geolocation';
 import { ClimaService } from '../../shared/clima.service';
 // import { LocalNotifications } from '@ionic-native/local-notifications';
 
-import { MapsAPILoader } from '@agm/core';
-// import {AutocompletePage} from './autocomplete';
-import { } from 'googlemaps';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -139,71 +118,15 @@ export class HomePage {
     map: GoogleMap;
     temperatura: number;
 
-<<<<<<< HEAD
-=======
-  map: GoogleMap;
-  temperatura: number;
-  searchControl: FormControl;
-  // address;
-  public latitude: number;
-  public longitude: number;
-  public zoom: number;
-<<<<<<< HEAD
-
-  @ViewChild("search")
-   public searchElementRef;
-
-  constructor(public navCtrl: NavController,
-    constructor(public navCtrl: NavController,
-    public toastCtrl: ToastController,
-    private geolocation: Geolocation,
-    private climaService: ClimaService) {
-    private climaService: ClimaService,
-     private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone,
-    private modalCtrl:ModalController
-=======
->>>>>>> c85444ad57ce97d6679775f34a4d334bc1448639
 
   constructor(public navCtrl: NavController,
     public toastCtrl: ToastController,
     private geolocation: Geolocation,
-<<<<<<< HEAD
     private climaService: ClimaService
     // private LocalNotifications:LocalNotifications,
     // public alertCtrl: AlertController,
     // private plt: Platform
   ) {
-=======
-    private climaService: ClimaService,
-     private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone
-    // private modalCtrl:ModalController
->>>>>>> e48c753071cd6573d82d989912bb10c2128cfbb0
-  ) {
-   //  this.address = {
-   //   place: ''
-   // };
-    this.zoom = 4;
-    this.latitude = 39.8282;
-    this.longitude = -98.5795;
-
-    //create search FormControl
-    this.searchControl = new FormControl();
-
-    //set current position
-    this.setCurrentPosition();
-<<<<<<< HEAD
-  }
-// showAddressModal () {
-//   let modal = this.modalCtrl.create(AutocompletePage);
-//   let me = this;
-//   modal.onDidDismiss(data => {
-//     this.address.place = data;
-//   });
-//   modal.present();
-=======
->>>>>>> c85444ad57ce97d6679775f34a4d334bc1448639
 
   //   this.plt.ready().then((rdy) =>{
   //     this.LocalNotifications.on('click',(notification,state) =>{
@@ -217,119 +140,10 @@ export class HomePage {
   //   }
   // );
   }
-<<<<<<< HEAD
-=======
-// showAddressModal () {
-//
-//
->>>>>>> e48c753071cd6573d82d989912bb10c2128cfbb0
-// }
-  ionViewDidLoad() {
-       //set google maps defaults
-       this.zoom = 4;
-       this.latitude = 39.8282;
-       this.longitude = -98.5795;
-<<<<<<< HEAD
-=======
-
-       //create search FormControl
-       this.searchControl = new FormControl();
-
-       //set current position
-       this.setCurrentPosition();
-
-       //load Places Autocomplete
-       this.mapsAPILoader.load().then(() => {
-           let nativeHomeInputBox = document.getElementById('txtHome').getElementsByTagName('input')[0];
-           let autocomplete = new google.maps.places.Autocomplete(nativeHomeInputBox, {
-               types: ["address"]
-           });
-           autocomplete.addListener("place_changed", () => {
-               this.ngZone.run(() => {
-                   //get the place result
-                   let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-                   //verify result
-                   if (place.geometry === undefined || place.geometry === null) {
-                       return;
-                   }
-
-                   //set latitude, longitude and zoom
-                   this.latitude = place.geometry.location.lat();
-                   this.longitude = place.geometry.location.lng();
-                   this.zoom = 12;
-               });
-           });
-       });
-   }
-
-     private setCurrentPosition() {
-         if ("geolocation" in navigator) {
-             navigator.geolocation.getCurrentPosition((position) => {
-                 this.latitude = position.coords.latitude;
-                 this.longitude = position.coords.longitude;
-                 this.zoom = 12;
-             });
-         }
-     }
-
->>>>>>> e48c753071cd6573d82d989912bb10c2128cfbb0
-
-       //create search FormControl
-       this.searchControl = new FormControl();
->>>>>>> c85444ad57ce97d6679775f34a4d334bc1448639
 
   loadMap(): void {
-       //set current position
-       this.setCurrentPosition();
 
 
-       //load Places Autocomplete
-       this.mapsAPILoader.load().then(() => {
-           let nativeHomeInputBox = document.getElementById('txtHome').getElementsByTagName('input')[0];
-           let autocomplete = new google.maps.places.Autocomplete(nativeHomeInputBox, {
-               types: ["address"]
-           });
-           autocomplete.addListener("place_changed", () => {
-               this.ngZone.run(() => {
-                   //get the place result
-                   let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-                   //verify result
-                   if (place.geometry === undefined || place.geometry === null) {
-                       return;
-                   }
-
-                   //set latitude, longitude and zoom
-                   this.latitude = place.geometry.location.lat();
-                   this.longitude = place.geometry.location.lng();
-                   this.zoom = 12;
-               });
-           });
-       });
-   }
-
-     private setCurrentPosition() {
-         if ("geolocation" in navigator) {
-             navigator.geolocation.getCurrentPosition((position) => {
-                 this.latitude = position.coords.latitude;
-                 this.longitude = position.coords.longitude;
-                 this.zoom = 12;
-                 this.climaService.getData(
-                     position.coords.latitude,
-                     position.coords.longitude
-                   );
-             });
-
-         }
-     }
-
-
-
-<<<<<<< HEAD
-  loadMap(): void {
-=======
->>>>>>> e48c753071cd6573d82d989912bb10c2128cfbb0
     this.geolocation.getCurrentPosition().then(position => {
       this.climaService.getData(
         position.coords.latitude,
@@ -359,10 +173,6 @@ export class HomePage {
         camera: {
           target: {
             lat: position.coords.latitude,
-<<<<<<< HEAD
-            lng: position.coords.longitude
-=======
->>>>>>> e48c753071cd6573d82d989912bb10c2128cfbb0
             lng: position.coords.longitude,
 
 
@@ -435,9 +245,4 @@ export class HomePage {
     this.navCtrl.push(BloqueadoresPage);
   }
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> e48c753071cd6573d82d989912bb10c2128cfbb0
 }
